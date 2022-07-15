ed9371eefe1d730b2a86f0233443920c7e6522b8
Vikas Konaparthi <44503709+Vikas-Konaparthi@users.noreply.github.com>
2022-07-15 11:24:25 +0530

    Version 4.0

--- kmc/app/src/main/java/com/example/kmc/CLogin/Reports.java
+++ kmc/app/src/main/java/com/example/kmc/CLogin/Reports.java
@@ -4,19 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.kmc.District;
 import com.example.kmc.Individual;
 import com.example.kmc.R;
 import com.example.kmc.SPElements;
 import com.example.kmc.SPOfficer;
+import com.example.kmc.Unit;
+import com.example.kmc.UnitElements;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -44,8 +51,15 @@ public class Reports extends AppCompatActivity {
     String village1;
     String village2;
     int sppending;
+    String district;
+    String const_selected;
+    public Spinner Constituency;
     ProgressBar progressBar;
     List<DocumentSnapshot> list2;
+    int unitCount=0;
+    int grounding=0;
+    ArrayList<UnitElements> unitList;
+
 
 
     @Override
@@ -53,9 +67,16 @@ public class Reports extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reports);
         db=FirebaseFirestore.getInstance();
+        Constituency = (Spinner) findViewById(R.id.spinner_constituency);
         datalist=new ArrayList<>();
+        unitList = new ArrayList<>();
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        progressBar.setVisibility(View.VISIBLE);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            district=extras.getString("district");
+            //The key argument here must match that used in the other activity
+        }
+//        progressBar.setVisibility(View.VISIBLE);
         db.collection("spofficer").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
@@ -63,6 +84,8 @@ public class Reports extends AppCompatActivity {
                         List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
                         for (DocumentSnapshot d : list) {
                             SPOfficer sp = d.toObject(SPOfficer.class);
+
+
                             db.collection("individuals").get()
                                     .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                                         @Override
@@ -74,19 +97,65 @@ public class Reports extends AppCompatActivity {
                                                 {
                                                     sppending=sppending+1;
                                                 }
+
                                             }
                                             SPElements spe=new SPElements(sp.getName(),sp.getVillage1(),sp.getVillage2(),String.valueOf(sppending));
                                             datalist.add(spe);
                                             sppending=0;
                                         }
+
+
                                     });
                         }
-                        progressBar.setVisibility(View.GONE);
+//                        progressBar.setVisibility(View.GONE);
                     }
                 });
+        List<String> constituencies = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_spinner_item,
+                constituencies);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Constituency.setAdapter(adapter);
+        db.collection(district+"_constituencies").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> list =queryDocumentSnapshots.getDocuments();
+                        for(DocumentSnapshot d:list) {
+
+                            District obj1 = d.toObject(District.class);
+
+                            obj1.setUid(d.getId().toString());
+                            constituencies.add(obj1.getUid());
+                        }
+                        adapter.notifyDataSetChanged();
+                        const_selected=adapter.getItem(0);
+                    }
+
+                });
+
+        Constituency.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,int position, long arg3) {
+                Constituency.setSelection(position);
+                const_selected = Constituency.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+
+        });
+
+        Toast.makeText(this, const_selected, Toast.LENGTH_SHORT).show();
     }
 
     public void spReport(View view) {
+        Log.d("SP111111", datalist.get(0).getName());
+        Log.d("SP222222", datalist.get(1).getName());
+
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT)
         {
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
@@ -123,15 +192,15 @@ public class Reports extends AppCompatActivity {
             int i=1;
             for(SPElements s:datalist)
             {
-                               Label name = new Label(0,i,s.getName());
-                                sheet.addCell(name);
-                               Label village = new Label(1,i,s.getVillage1());
+                Label name = new Label(0,i,s.getName());
+                sheet.addCell(name);
+                Label village = new Label(1,i,s.getVillage1());
                 sheet.addCell(village);
-                                Label village2 = new Label(2,i,s.getVillage2());
+                Label village2 = new Label(2,i,s.getVillage2());
                 sheet.addCell(village2);
                 Label pending = new Label(3,i,s.getPending());
                 sheet.addCell(pending);
-                                i++;
+                i++;
             }
 
 
@@ -155,6 +224,69 @@ public class Reports extends AppCompatActivity {
     }
 
     public void unitReport(View view) {
+//        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT)
+//        {
+//            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+//        }
+//        String Fnamexls="Unit_wise_report"+System.currentTimeMillis()+ ".xls";
+//        File dir = Environment.getExternalStoragePublicDirectory("kmc");
+//        String filePath = Environment.getExternalStorageDirectory()+File.separator+"kmc";
+//        try{
+//            dir.mkdirs();
+//        }catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//
+//        File file = new File(dir, Fnamexls);
+//
+//        WorkbookSettings wbSettings = new WorkbookSettings();
+//        wbSettings.setLocale(new Locale("en", "EN"));
+//
+//        WritableWorkbook workbook;
+//
+//        try {
+//            workbook = Workbook.createWorkbook(file, wbSettings);
+//            //workbook.createSheet("Report", 0);
+//            WritableSheet sheet = workbook.createSheet("First Sheet", 0);
+//            Label label0 = new Label(0, 0, "Unit Name");
+//            Label label1 = new Label(1, 0, "Total NO of Units");
+//            Label label2 = new Label(2, 0, "Grounded");
+//            sheet.addCell(label0);
+//            sheet.addCell(label1);
+//            sheet.addCell(label2);
+//            int i=1;
+//            for(UnitElements s:unitList)
+//            {
+//                Label unitName = new Label(0,i,s.getUnitName());
+//                sheet.addCell(unitName);
+//                Label unitCount = new Label(1,i,s.getUnitCount());
+//                sheet.addCell(unitCount);
+//                Label grounding = new Label(2,i,s.getGrounding());
+//                sheet.addCell(grounding);
+//                i++;
+//            }
+//
+//
+//            workbook.write();
+//            try {
+//                workbook.close();
+//            } catch (WriteException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//        }catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        } catch (RowsExceededException e) {
+//            e.printStackTrace();
+//        } catch (WriteException e) {
+//            e.printStackTrace();
+//        }
+//        Toast.makeText(this, "Unit wise Report Generated Successfully", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, Demo.class);
+        i.putExtra("constituency",const_selected);
+        startActivity(i);
 
     }
 }
